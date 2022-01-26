f396175836a5e28c7a1c139c7bda24cbe7df9d94
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-01-26 19:19:06 +0600

    Initial Commit

---
+++ ChartBar/ChartBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartBarApp.swift
+//  ChartBar
+//
+//  Created by Sopnil Sohan on 26/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ChartBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
